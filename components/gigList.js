be696645670d9968b8a0f5336c34a542b9b71ece
Michael Sampson <m.andresampson@gmail.com>
2021-08-18 14:16:46 -0700

    Added all months

--- components/gigList.js
+++ components/gigList.js
@@ -2,6 +2,36 @@ import GigCard from "./gigCard";
 import styles from "../styles/components/gigList.module.css";
 
 function GigList(props) {
+  let januaryArray = props.gigs
+    .filter((gig) => gig.metadata.month === "01")
+    .sort(function (a, b) {
+      return parseInt(a.metadata.day) - parseInt(b.metadata.day);
+    });
+  let februaryArray = props.gigs
+    .filter((gig) => gig.metadata.month === "02")
+    .sort(function (a, b) {
+      return parseInt(a.metadata.day) - parseInt(b.metadata.day);
+    });
+  let marchArray = props.gigs
+    .filter((gig) => gig.metadata.month === "03")
+    .sort(function (a, b) {
+      return parseInt(a.metadata.day) - parseInt(b.metadata.day);
+    });
+  let aprilArray = props.gigs
+    .filter((gig) => gig.metadata.month === "04")
+    .sort(function (a, b) {
+      return parseInt(a.metadata.day) - parseInt(b.metadata.day);
+    });
+  let mayArray = props.gigs
+    .filter((gig) => gig.metadata.month === "05")
+    .sort(function (a, b) {
+      return parseInt(a.metadata.day) - parseInt(b.metadata.day);
+    });
+  let juneArray = props.gigs
+    .filter((gig) => gig.metadata.month === "06")
+    .sort(function (a, b) {
+      return parseInt(a.metadata.day) - parseInt(b.metadata.day);
+    });
   let julyArray = props.gigs
     .filter((gig) => gig.metadata.month === "07")
     .sort(function (a, b) {
@@ -17,8 +47,38 @@ function GigList(props) {
     .sort(function (a, b) {
       return parseInt(a.metadata.day) - parseInt(b.metadata.day);
     });
+  let octoberArray = props.gigs
+    .filter((gig) => gig.metadata.month === 10)
+    .sort(function (a, b) {
+      return parseInt(a.metadata.day) - parseInt(b.metadata.day);
+    });
+  let novemberArray = props.gigs
+    .filter((gig) => gig.metadata.month === 11)
+    .sort(function (a, b) {
+      return parseInt(a.metadata.day) - parseInt(b.metadata.day);
+    });
+  let decemberArray = props.gigs
+    .filter((gig) => gig.metadata.month === 12)
+    .sort(function (a, b) {
+      return parseInt(a.metadata.day) - parseInt(b.metadata.day);
+    });
 
-  let monthsArray = [...julyArray, ...augustArray, ...septemberArray];
+  let monthsArray = [
+    ...januaryArray,
+    ...februaryArray,
+    ...marchArray,
+    ...aprilArray,
+    ...mayArray,
+    ...juneArray,
+    ...julyArray,
+    ...augustArray,
+    ...septemberArray,
+    ...octoberArray,
+    ...novemberArray,
+    ...decemberArray,
+  ];
+
+  console.log(props.gigs);
 
   let content =
     monthsArray.length >= 1 ? (
