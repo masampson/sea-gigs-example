6149ddb6009f4e2174467156067004a8012140e8
Mike Sampson <mike.sampson@rocketinsights.com>
2022-03-22 15:26:16 -0700

    Internal Submit Email fix
    make login email associated by default

--- pages/index.js
+++ pages/index.js
@@ -111,6 +111,10 @@ function SeaGigs({ gigs }) {
         <meta charSet="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="SeaGigs - Seattle's premier show calendar."
+        />
         {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
